e3774222f6a6656dc5c7ac41a825f258de287a47
ohongs <samkahunga65@gmail.com>
2020-05-27 16:59:01 +0300

    j

--- shuleonline/shuleonline/settings.py
+++ shuleonline/shuleonline/settings.py
@@ -133,6 +133,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'build', "static"),
 )
