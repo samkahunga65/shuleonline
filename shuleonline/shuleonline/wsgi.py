a949c6cd025a45143d91d1919ba8ae0de83acee5
ohongs <samkahunga65@gmail.com>
2020-05-28 12:25:42 +0300

    d

--- shuleonline/shuleonline/wsgi.py
+++ shuleonline/shuleonline/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shuleonline.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shuleonline.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
